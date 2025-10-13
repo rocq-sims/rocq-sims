6cd5e7d429c5650890f2a686a906e8beabdebb7d
Nicolas Chappe <nicolas.chappe@univ-grenoble-alpes.fr>
2025-10-13 15:31:51 +0200

    Add a notion of predicate valid from a given state

--- theories/LTS.v
+++ theories/LTS.v
@@ -12,7 +12,10 @@ From RelationAlgebra Require Import
      rewriting
      normalisation
      monoid.
+From Coinduction Require Import all.
 From Sims Require Import Utils.
+
+Import CoindNotations.
 
 Variant label {S : Type} := obs (s : S) | tau.
 
@@ -110,9 +113,14 @@ Qed.
 Definition is_stuck (st : lts.(St)) : Prop :=
   ~ extrans st.
 
-#[export] Instance : Proper (lts.(St).(Eq) ==> flip impl) is_stuck.
+#[export] Instance : Proper (lts.(St).(Eq) ==> impl) is_stuck.
 Proof.
-  cbn. unfold is_stuck. intros. now setoid_rewrite H.
+  cbn. unfold is_stuck. intros. now setoid_rewrite <- H.
+Qed.
+
+#[export] Instance : Proper (lts.(St).(Eq) ==> iff) is_stuck.
+Proof.
+  intros. apply proper_sym_impl_iff; typeclasses eauto.
 Qed.
 
 Definition can_be_stuck s :=
@@ -262,7 +270,91 @@ Proof.
       exists x. split; auto. now rewrite <- str_itr'.
 Qed.
 
+
+(* From P st : all the states reachable from st verify property P. *)
+
+Program Definition fromF P : mon (lts.(St) -> Prop) :=
+  {| body := fun P' st => P st /\ forall l st', trans l st st' -> P' st' |}.
+Next Obligation.
+  eauto.
+Qed.
+
+#[export] Instance fromF_eq : forall P P',
+  Proper (lts.(St).(Eq) ==> impl) P ->
+  Proper (lts.(St).(Eq) ==> impl) (fromF P P').
+Proof.
+  intros. cbn. intros. destruct H1. split.
+  now rewrite <- H0.
+  intros. eapply H2. rewrite H0. apply H3.
+Qed.
+
+Definition From P := gfp (fromF P).
+
+#[export] Instance Chain_fromF_eq : forall P (C : Chain (fromF P)),
+  Proper (lts.(St).(Eq) ==> impl) P ->
+  Proper (lts.(St).(Eq) ==> impl) (`C).
+Proof.
+  intros ???. apply tower; clear C.
+  { intros ????????. eapply H0; eauto. }
+  intros C **. cbn. intros. rewrite <- H1. split; auto.
+  now destruct H2.
+  intros. eapply H2. rewrite H1. apply H3.
+Qed.
+
+#[export] Instance From_eq : forall P,
+  Proper (lts.(St).(Eq) ==> impl) P ->
+  Proper (lts.(St).(Eq) ==> impl) (From P).
+Proof.
+  intros. typeclasses eauto.
+Qed.
+
+Lemma forall_from : forall (P : lts.(St) -> Prop),
+  (forall st, P st) -> forall st, From P st.
+Proof.
+  intros ??. red. coinduction R CH. intros.
+  split; auto.
+Qed.
+
+Lemma from_id : forall [P : lts.(St) -> Prop] [st],
+  From P st ->
+  P st.
+Proof.
+  intros. now apply (gfp_fp (fromF _)) in H as [].
+Qed.
+
+Lemma from_trans : forall (P : lts.(St) -> Prop) l st st',
+  From P st ->
+  trans l st st' ->
+  From P st'.
+Proof.
+  intros. apply (gfp_fp (fromF _)) in H as []. eapply H1; eauto.
+Qed.
+Hint Resolve from_trans : optsim.
+
+Lemma from_taustar : forall (P : lts.(St) -> Prop) (P_Eq: Proper (lts.(St).(Eq) ==> impl) P) st st',
+  From P st ->
+  (trans tau)^* st st' ->
+  From P st'.
+Proof.
+  intros.
+  eapply srel_str_ind_r with (P := From P); eauto.
+  typeclasses eauto.
+  clear st st' H H0.
+  intros. apply (gfp_fp (fromF P)) in H0 as [].
+  apply H1 in H. apply H.
+Qed.
+
+
 End LTS.
+
+Lemma can_be_stuck_delay : forall {lts} (s : lts.(St)),
+  can_be_stuck SimOpt.delay s ->
+  exists s', (trans tau)^* s s' /\ is_stuck s'.
+Proof.
+  intros. destruct H.
+  - exists s. split; auto. now rewrite <- str_refl.
+  - apply H.
+Qed.
 
 Lemma tauplus_dtrans {lts} : forall s s',
   (lts.(trans) tau)^+ s s' -> dtrans SimOpt.delay tau s s'.
@@ -279,3 +371,5 @@ Hint Unfold can_be_stuck : optsim.
 Hint Resolve trans_dtrans : optsim.
 (*Hint Resolve delay_trans_dtrans : optsim.*)
 Hint Resolve dtrans_trans : optsim.
+
+Hint Resolve from_trans : optsim.
